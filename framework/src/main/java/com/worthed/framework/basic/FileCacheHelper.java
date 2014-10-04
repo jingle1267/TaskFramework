6f0e3fd463fd94b28edfe9c5eecb0031bedd4b0a
Zhenguo <jingle1267@163.com>
2014-10-04 10:16:12 +0800

    [NewFeature] Add file cache helper.

---
+++ framework/src/main/java/com/worthed/framework/basic/FileCacheHelper.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014 zhenguo (jingle1267@163.com)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.worthed.framework.basic;
+
+import android.content.Context;
+
+import java.io.File;
+
+/**
+ * 文件缓存工具类
+ * Created by jingle1267@163.com on 14-10-4.
+ */
+public class FileCacheHelper {
+
+    public static final String CACHE_FILE_DIR = "/cache/object/";
+
+    public static void deleteAllCache(Context context) {
+
+    }
+
+    public static void saveObj2File(Context context, String fileName, Object obj) {
+
+    }
+
+    public static void getObjFromFile(Context context, String fileName) {
+
+    }
+
+    private static void save(String path, Object obj) {
+
+    }
+
+    private static void read(String path) {
+
+    }
+
+    private static void deleteAll(File file) {
+
+    }
+
+    private static String getPath() {
+
+        return null;
+    }
+
+}
